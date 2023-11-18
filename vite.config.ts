337552c03ed56a26db6c479663dec8a7e58f1b36
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-11-18 10:58:14 +0900

    chore: experiment with multiple entry

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@ import { unstable_vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
+export default defineConfig((env) => ({
   plugins: [remix(), tsconfigPaths()],
-});
+  build: env.isSsrBuild ? {
+    rollupOptions: {
+      input: ["./app/other-entry.ts"],
+    }
+  } : {}
+}));
